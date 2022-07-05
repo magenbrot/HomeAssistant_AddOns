eb506de74e4c1235149801353d92667d86a4b393
Oliver Völker <info@ovtec.it>
2022-07-05 15:14:36 +0200

    apply shellcheck recommendations

--- local_audio_play/run.sh
+++ local_audio_play/run.sh
@@ -1,20 +1,18 @@
 #!/command/with-contenv bashio
+# shellcheck shell=bash
+
 set -e
 
 bashio::log.info "local_audio_play starting"
 
 VOLUME=$(bashio::config 'volume')
 bashio::log.info "Setting volume to ${VOLUME}"
-amixer -q sset 'Master' ${VOLUME}
+amixer -q sset 'Master' "${VOLUME}"
 
-# Read from STDIN aliases to play file
 while read -r input; do
-  # removing JSON stuff
-  #input="$(echo "$input" | bashio::jq '.')"
-  input=$(bashio::jq $input '.')
-  bashio::log.info "Read alias: $input"
-
-  if ! msg="$(mpg123 $input)"; then
-    bashio::log.error "Playing failed -> $msg"
+  input=$(bashio::jq "${input}" '.')
+  bashio::log.info "Stdin: ${input}"
+  if ! msg=$(mpg123 "${input}"); then
+    bashio::log.error "Playing failed: ${msg}"
   fi
 done
